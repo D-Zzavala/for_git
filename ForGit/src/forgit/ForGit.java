0b9c6ec035e396cd27f60847e12483c947339242
D-Zzavala <65042896+D-Zzavala@users.noreply.github.com>
2020-10-22 19:37:45 -0600

    Hello world

--- ForGit/src/forgit/ForGit.java
+++ ForGit/src/forgit/ForGit.java
@@ -17,13 +17,11 @@ public class ForGit {
     
     public static void main(String[] args) {
         
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
+        System.out.println("Practicas");
+        for (int i = 0; i < 10; i++) {
+            
+        System.out.println("Hello World");
+        }
         
     }
     
