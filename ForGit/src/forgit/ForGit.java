99285ea4c9b6a2eb44e3e1983ca4bd0e52d36f1a
D-Zzavala <65042896+D-Zzavala@users.noreply.github.com>
2020-10-22 19:34:30 -0600

    scanner hecho

---
+++ ForGit/src/forgit/ForGit.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package forgit;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 50431
+ */
+public class ForGit {
+
+    static Scanner read = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        
+    }
+    
+}
